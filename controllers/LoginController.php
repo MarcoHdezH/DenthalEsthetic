1a75a6430e16013b8430fee24343661ffcedf7c8
Marco Hernandez <marco7245@hotmail.com>
2023-05-31 19:18:06 -0600

    Creacion de Rutas de Autenticacion

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+    public static function login(){
+        echo "Desde Login";
+    }
+
+    public static function logout(){
+        echo "Desde Logout";
+    }
+
+    public static function olvidar(){
+        echo "Desde Olvidar";
+    }
+
+    public static function recuperar(){
+        echo "Desde Recuperar";
+    }
+
+    public static function crear(){
+        echo "Desde Crear";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,9 +2,25 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
+
+//Iniciar Sesion
+$router->get('/',[LoginController::class,'login']);
+$router->post('/',[LoginController::class,'login']);
+$router->get('/logout',[LoginController::class,'logout']);
+
+//Recuperar Contraseñas
+$router->get('/olvidar',[LoginController::class,'olvidar']);
+$router->post('/olvidar',[LoginController::class,'olvidar']);
+$router->get('/recuperar',[LoginController::class,'recuperar']);
+$router->post('/recuperar',[LoginController::class,'recuperar']);
+
+//Crear Cuentas
+$router->get('/crear-cuenta',[LoginController::class,'crear']);
+$router->post('/crear-cuenta',[LoginController::class,'crear']);
 
 
 
