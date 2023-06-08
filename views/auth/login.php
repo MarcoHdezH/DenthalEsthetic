a09e248504baf0a4714c353b359e25c27812c5ff
Marco Hernandez <marco7245@hotmail.com>
2023-06-07 20:58:06 -0600

    aAñadiendo Primeros Estilos

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,22 @@
 <h1 class="nombre-pagina">¡Bienvenid@!</h1>
-
 <p class="descripcion-pagina">Agenda tu experiencia en pocos minutos</p>
+
+<form class="formulario" method="POST" action="/">
+    
+    <div class="campo">
+        <label for="email">Correo:</label>
+        <input type="email" id="email" placeholder="ejemplo@correo.com" name="email">
+    </div>
+
+    <div class="campo">
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" placeholder="Contraseña" name="password">
+    </div>
+
+    <input type="submit" class="boton" value="iniciar Sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una Cuenta? ¡Unete hoy mismo!</a>
+    <a href="/crear-cuenta">¿Olvidaste tu Contraseña?</a>
+</div>
