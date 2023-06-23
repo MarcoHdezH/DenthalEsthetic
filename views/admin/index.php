cf11f634f56c6e60ce8a741957142154eaa4867e
Marco Hernandez <marco7245@hotmail.com>
2023-06-22 19:51:41 -0600

    Consulta de Citas en DB

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,3 +1,26 @@
 <h1 class="nombre-pagina">Panel de Administración</h1>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+
+<h2>Buscar Citas</h2>
+
+<div class="busqueda">
+    <form class="formulario">
+        <div class="campo">
+            <label for="fecha">Fecha</label>
+            <input type="date" id="fecha" name="fecha">
+        </div>
+    </form>
+</div>
+
+<div id="citas-admin"></div>
+
+<!-- 
+    SELECT * FROM citas LLEFT OUTER JOIN usuarios
+    ON citas.usuarioID=usuarios.id
+    LEFT OUTER JOIN citasservicios
+    ON citasservicios.citaID=citas.id
+    LEFT OUTER JOIN servicios
+    ON servicios.id=citasservicios.servicioID
+    WHERE fecha ="2023-07-07"
+ -->
