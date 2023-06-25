32ae0d7dc90c26d42b80de40203d8a3bade4f194
Marco Hernandez <marco7245@hotmail.com>
2023-06-24 22:09:39 -0600

    Proteccion de Rutas

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -11,6 +11,8 @@ class ServicioController{
 
         session_start();
 
+        isAdmin();
+
         $servicios = Servicio::all();
 
         $router->render('/servicios/index',[
@@ -21,6 +23,8 @@ class ServicioController{
 
     public static function crear(Router $router){
         session_start();
+
+        isAdmin();
 
         $servicio = new Servicio;
         $alertas = [];
@@ -46,6 +50,8 @@ class ServicioController{
 
         session_start();
 
+        isAdmin();
+
         if(!is_numeric($_GET['id'])) return;
 
         $servicio = Servicio::find($_GET['id']);
@@ -70,6 +76,9 @@ class ServicioController{
     }
 
     public static function eliminar(){
+        session_start();
+        isAdmin();
+
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $id = $_POST['id'];
             $servicio = Servicio::find($id);
