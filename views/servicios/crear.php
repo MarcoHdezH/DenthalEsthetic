5847f8e66a5f5e76e3986404d39cf3873925971f
Marco Hernandez <marco7245@hotmail.com>
2023-06-24 21:17:35 -0600

    Mostrar Lista de Servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -2,7 +2,7 @@
 <p class="descripcion-pagina">Crea un nuevo Servicio para el Consultorio</p>
 
 <?php
-    // include_once __DIR__.'/../templates/barra.php';
+    include_once __DIR__.'/../templates/barra.php';
     include_once __DIR__.'/../templates/alertas.php';
 ?>
 
--- views/servicios/index.php
+++ views/servicios/index.php
@@ -4,3 +4,11 @@
 <?php
     include_once __DIR__.'/../templates/barra.php';
 ?>
+
+<ul class="servicios">
+    <?php foreach($servicios as $servicio){?>
+        <li>
+            <p>Nombre: <span> <?php echo $servicio->nombre ?></span> </p>
+        </li>
+    <?php } ?>
+</ul>
